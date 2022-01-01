07793c44d8411a116fd1bdf6784b6f8c3ea9ce5d
hilamedina <hm18986@gmail.com>
2022-01-01 23:21:47 +0200

    anatomy

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navbar">
-      <Link className="Link" to="/Log in">
+      <Link className="Link" to="/Login">
         Log in
       </Link>
       <Link className="Link" to="/Body">
